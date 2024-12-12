5b2966f506772506a5c8ead1f29e02d06e879ff3
Isac Epifanio <isacepifanio4@gmail.com>
2024-12-12 20:21:56 -0300

    morganMiddleware configurado com sucesso

--- config/logges.ts
+++ config/logges.ts
@@ -23,30 +23,32 @@ const colors = {
   debug: "white",
 };
 
-winston.addColors(colors)
+winston.addColors(colors);
 
 const format = winston.format.combine(
-    winston.format.timestamp({format: 'YYYY-MM-DD HH-mm-ss-ms'}),
-    winston.format.colorize({all: true}),
-    winston.format.printf((info) => `${info.timestamp} - ${info.level} - ${info.message}`)
-)
+  winston.format.timestamp({ format: "YYYY-MM-DD HH-mm-ss-ms" }),
+  winston.format.colorize({ all: true }),
+  winston.format.printf(
+    (info) => `${info.timestamp} - ${info.level} - ${info.message}`
+  )
+);
 
 const transports = [
-    new winston.transports.Console(),
-    new winston.transports.File({
-        filename: 'logs/error.log',
-        level: 'error'
-    }),
-    new winston.transports.File({
-        filename: 'logs/all.log'
-    })
-]
+  new winston.transports.Console(),
+  new winston.transports.File({
+    filename: "logs/error.log",
+    level: "error",
+  }),
+  new winston.transports.File({
+    filename: "logs/all.log",
+  }),
+];
 
 const logger = winston.createLogger({
-    level: level(),
-    levels,
-    format,
-    transports
-})
+  level: level(),
+  levels,
+  format,
+  transports,
+});
 
-export default logger
+export default logger;
